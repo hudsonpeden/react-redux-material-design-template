ec670c499f450771babcec4419a630c884351363
Hudson Peden <hpeden@logicalsysinc.com>
2017-10-23 15:54:09 -0500

    AppBar with persistent Drawer added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 
 import '../style/App.css';
-import HeaderBar from './HeaderBar';
+import Menu from './menu/Menu';
+
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { red, blue, yellow } from 'material-ui/colors';
+
+
+/** 
+ * 
+ * GLOBAL APPLICATION THEME!
+ * 
+*/
+const theme = createMuiTheme
+({
+  palette: 
+  {
+    primary: blue,
+    secondary: yellow,
+    error: red 
+  }
+});
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HeaderBar/>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Menu/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
